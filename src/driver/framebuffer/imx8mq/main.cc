6b6b7f9322c65e0d5e9d941d450a104fb3efc25f
Josef Söntgen <josef.soentgen@genode-labs.com>
2025-01-23 16:31:02 +0100

    WIP: framebuffer/imx8mq: use blit library to rotate

--- src/driver/framebuffer/imx8mq/main.cc
+++ src/driver/framebuffer/imx8mq/main.cc
@@ -42,81 +42,12 @@ struct Framebuffer::Driver
 			Capture::Area const         _size;
 			Capture::Connection::Screen _captured_screen;
 			void                      * _base;
-			bool                        _ccw; /* blit 90 deg counterclockwise */
 
 			/*
 			 * Noncopyable
 			 */
 			Fb(const Fb&);
 			Fb & operator=(const Fb&);
-
-			struct Pixel_access
-			{
-				Capture::Area const _size;
-				Capture::Pixel     *_addr;
-
-				Pixel_access(Capture::Area const size,
-				             Capture::Pixel *addr)
-				: _size(size), _addr(addr) { }
-
-				Capture::Pixel *at(int x, int y)
-				{
-					return _addr + y * _size.w + x;
-				}
-
-				Capture::Pixel *at_ccw(int x, int y)
-				{
-					return _addr + (_size.w - x - 1) * _size.h + y;
-				}
-			};
-
-			void _blit_ccw(Surface<Capture::Pixel> &surface,
-			               Texture<Capture::Pixel> const &texture)
-			{
-				using Rect = Capture::Rect;
-				Rect const clipped = Rect::intersect(Rect({0, 0}, texture.size()),
-				                                     surface.clip());
-
-				if (!clipped.valid())
-					return;
-
-				Pixel_access src { _size, const_cast<Capture::Pixel *>(texture.pixel()) };
-				Pixel_access dst { _size, surface.addr()  };
-
-				Capture::Pixel *spx = src.at(clipped.x1(), clipped.y1());
-				Capture::Pixel *dpx = dst.at_ccw(clipped.x1(), clipped.y1());
-
-				/*
-				 * BIG time optimization necessary, at least avoid any expansive
-				 * operation (like mul/div etc.) for now
-				 */
-				size_t w = 0;
-
-				/* col and row refer to src, they have inverted meaning at dst */
-				for (unsigned row = 0; row < clipped.h(); row++, w += _size.w) {
-					size_t h = 0;
-					for (unsigned col = 0; col < clipped.w(); col++, h += _size.h)
-						(dpx - h + row)->pixel = (spx + w + col)->pixel;
-				}
-			}
-
-			void _apply_to_surface_ccw(Surface<Capture::Pixel> &surface)
-			{
-				using Pixel = Capture::Pixel;
-				using Affected_rects = Capture::Session::Affected_rects;
-
-				Affected_rects const affected = _capture.capture_at(Capture::Point(0, 0));
-
-				_captured_screen.with_texture([&] (Texture<Pixel> const &texture) {
-
-					affected.for_each_rect([&] (Capture::Rect const rect) {
-
-						surface.clip(rect);
-
-						_blit_ccw(surface, texture);
-					});
-				});
-			}
 
 		public:
 
@@ -125,23 +56,23 @@ struct Framebuffer::Driver
 				using Pixel = Capture::Pixel;
 				Surface<Pixel> surface((Pixel*)_base, _size);
 
-				if (_ccw)
-					_apply_to_surface_ccw(surface);
-				else
-					_captured_screen.apply_to_surface(surface);
+				_captured_screen.apply_to_surface(surface);
 			}
 
 
-			Fb(Env & env, void * base, unsigned xres, unsigned yres, bool ccw)
+			Fb(Env & env, void * base, unsigned xres, unsigned yres, bool r90)
 			:
 				_capture(env),
-				_size{ ccw ? yres : xres, ccw ? xres : yres },
-				_captured_screen(_capture, env.rm(), { .px     = _size,
-				                                       .mm     = { },
-				                                       .rotate = { },
-				                                       .flip   = { } }),
-				_base(base),
-				_ccw(ccw) {}
+				_size(xres, yres),
+				_captured_screen(_capture, env.rm(),
+				                 { .px = r90 ? Blit::transformed(_size, Blit::Rotate::R90)
+				                             : _size,
+				                   .mm = { },
+				                   .rotate = r90 ? Blit::Rotate::R90
+				                                 : Blit::Rotate::R0,
+				                   .flip = { } }),
+				_base(base)
+			{ }
 	};
 
 	enum { MAX_SCREENS = 2 };
@@ -203,12 +134,12 @@ extern "C" void lx_emul_framebuffer_ready(void * base, unsigned long,
 			continue;
 
 		/* rotate */
-		bool ccw = false;
+		bool r90 = false;
 
-		/* check for MNT Pocket Reform in device tree */
-		if (lx_emul_machine_is_compatible("mntre,pocket-reform")) ccw = true;
+		/* check for Armstone in device tree */
+		if (lx_emul_machine_is_compatible("fus,imx8mp-armstonemx8mp")) r90 = true;
 
-		driver(env).fb[i].construct(env, base, xres, yres, ccw);
+		driver(env).fb[i].construct(env, base, xres, yres, r90);
 
 		Genode::log("--- i.MX 8MQ framebuffer driver screen ", i, " initialized ---");
 		return;
